b7e3fa7cd549263db4afcc32705748e3bc4d45f2
mcdy143 <mcdy143@gmail.com>
2015-02-11 21:16:23 +0000

    single-end reads working for edena, paired end wrangle function is not correct due to prinseq filenames have random characters append to it

--- module_edena.py
+++ module_edena.py
@@ -4,8 +4,8 @@ __author__ = 'mcdy143'
 import os
 import subprocess
 
-inputDirectory = "/home/biol8803b/tmp/"
-outputDirectory = "/home/biol8803b/assemblies/"
+inputDirectory = "/home/sim8/assemblyMagicResults/prinseq/"
+outputDirectory = "/home/sim8/assemblyMagicResults/edena/"
 paths = [os.path.join(inputDirectory,fn) for fn in next(os.walk(inputDirectory))[2]]
 
 fileHash = {}
@@ -13,25 +13,32 @@ fileHash = {}
 #Generates a hashmap of 'key : value' pairs out of file names and paths.
 def wranglePairedEnds(paths):
     for file in paths:
+        print file
         newfile = ""
         if "R1" in file:
             newfile = file.replace("R1", "R*")
+            print "newFile, R1 --", newfile
         elif "R2" in file:
             newfile = file.replace("R2", "R*")
+            print "newFile, R2 --", newfile
         if not newfile in fileHash:
+            print "newFile, notin --", newfile
             fileHash[newfile] = [file]
         else:
+            print "already in newfile --", newfile
             fileHash[newfile].append(file)
     return
 
 def moduleedenaSE(file):
     base = os.path.basename(file)
     filename = os.path.splitext(base)[0]
-    subprocess.call(["spades.py", "--careful", "-s", file, "-o", outputDirectory+filename+".spades",])
+    fileOutputDirectory = outputDirectory+str(filename)
+    subprocess.call(["mkdir", fileOutputDirectory])
+    subprocess.call(["edena", "-r", file, "-p", fileOutputDirectory+"/out"])
+    subprocess.call(["edena", "-e", fileOutputDirectory+"/out.ovl", "-p", fileOutputDirectory+"/"])
 
 def moduleedenaPE(inputfileone, inputfiletwo):
-    subprocess.call(["spades.py", "--careful", "-1", inputfileone, "-2", inputfiletwo, "-o", outputDirectory+ ])
+    subprocess.call(["spades.py", "--careful", "-1", inputfileone, "-2", inputfiletwo, "-o", outputDirectory])
 
-for file in paths:
-
-    moduleSpadesSE(file)
+for file in paths[:2]:
+    moduleedenaSE(file)

d336d96e389edbb2106bab0ee0604a4fcfa07a51
xin-w <xin-w@users.noreply.github.com>
2015-02-11 12:15:31 -0500

    Update module_abyss.py

--- module_abyss.py
+++ module_abyss.py
@@ -3,8 +3,8 @@ import sys
 import subprocess
 import re
 
-inputDirectory = '/home/sim8/assemblyMagicResults/prinseq'
-outputDirectory = '/home/sim8/assmeblyMagicResults/abyss'
+inputDirectory = '/home/sim8/assemblyMagicResults/prinseq/'
+outputDirectory = '/home/sim8/assmeblyMagicResults/abyss/'
 
 paths = [os.path.join(inputDirectory,fn) for fn in next(os.walk(inputDirectory))[2]]
 

69cfc7d7971c43ec62f4480d5a27b23c46d646c1
mcdy143 <mcdy143@gmail.com>
2015-02-15 04:50:34 +0000

    adding quast module

---
+++ module_quast.py
@@ -0,0 +1,16 @@
+__author__ = 'mcdy143'
+#!/usr/bin/python3                                                                                                                                                                                           
+
+import os
+import subprocess
+
+inputDirectory_edena = "/home/sim8/assemblyMagicResults/edena/"
+inputDirectory_abyss = "/home/sim8/assemblyMagicResults/abyss/"
+inputDirectory_spades = "/home/sim8/assemblyMagicResults/spades/"
+folders_edena = [os.path.join(inputDirectory_edena,fn) for fn in next(os.walk(inputDirectory_edena))[1]]
+folders_abyss = [os.path.join(inputDirectory_abyss,fn) for fn in next(os.walk(inputDirectory_abyss))[1]]
+folders_spades = [os.path.join(inputDirectory_spades,fn) for fn in next(os.walk(inputDirectory_spades))[1]]
+
+for folder in folders_edena:
+    filePath = folder + "/_contigs.fasta"
+    subprocess.call(["/home/agupta399/installation/quast-2.3/quast.py", "-o", folder, filePath])

a167634df90164c6a0f4b31687f93486d6f19fc9
mcdy143 <mcdy143@gmail.com>
2015-02-23 18:00:09 +0000

    quast module has been completed and ran on de novo assemblies

--- module_quast.py
+++ module_quast.py
@@ -2,6 +2,7 @@ __author__ = 'mcdy143'
 #!/usr/bin/python3
 import os
 import subprocess
+import re
 
 inputDirectory_edena = "/home/sim8/assemblyMagicResults/edena/"
 inputDirectory_abyss = "/home/sim8/assemblyMagicResults/abyss/"
@@ -10,16 +11,15 @@ folders_edena = [os.path.join(inputDirectory_edena,fn) for fn in next(os.walk(in
 folders_abyss = [os.path.join(inputDirectory_abyss,fn) for fn in next(os.walk(inputDirectory_abyss))[1]]
 folders_spades = [os.path.join(inputDirectory_spades,fn) for fn in next(os.walk(inputDirectory_spades))[1]]
 
-'''
 for folder in folders_edena:
     filePath = folder + "/_contigs.fasta"
     subprocess.call(["/home/agupta399/installation/quast-2.3/quast.py", "-o", folder, filePath])
-'''
+
 for folder in folders_spades:
     filePath = folder + "/scaffolds.fasta"
     subprocess.call(["/home/agupta399/installation/quast-2.3/quast.py", "-o", folder, filePath])
-'''
+
 for folder in folders_abyss:
-    filePath = folder + "/contigs.fasta"
+    filePath = folder + "/scaffolds.fa"
     subprocess.call(["/home/agupta399/installation/quast-2.3/quast.py", "-o", folder, filePath])
-'''
+
